67b6b45602744654e43068ce40d823d31ba9d109
adakeefer <adamjkeefer@gmail.com>
2025-05-06 14:16:37 -0400

    chore: add component __init__.py files

---
+++ crawler/components/url_frontier/__init__.py
@@ -0,0 +1,4 @@
+"""
+URL Frontier component for web crawler.
+Manages and distributes URLs to worker instances.
+""" 
---
+++ crawler/components/worker/__init__.py
@@ -0,0 +1,4 @@
+"""
+Worker component for web crawler.
+Handles downloading web pages, parsing content, and extracting new URLs.
+""" 
